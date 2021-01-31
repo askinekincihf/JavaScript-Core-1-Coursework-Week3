c95a4ae6240ce90271d15a2f9c88d8e8b65901c4
askinekinci <askinekincihf@gmail.com>
2021-01-31 22:08:50 +0000

    completed E-array-map

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -2,3 +2,17 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+
+let multiplyNumbersBy100 = numbers.map(num => num * 100);
+
+let multiplyNumbers = numbers.map(num => {
+    return num * 100
+});
+
+let multiplyNum = numbers.map(function (num) {
+    return num * 100
+});
+
+console.log(multiplyNumbersBy100);
+console.log(multiplyNumbers);
+console.log(multiplyNum);
