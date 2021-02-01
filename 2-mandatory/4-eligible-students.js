d285189c2860f4b3c15b137bd80803a912fc6e43
askinekinci <askinekincihf@gmail.com>
2021-02-01 21:17:21 +0000

    completed 4-eligible-students

--- 2-mandatory/4-eligible-students.js
+++ 2-mandatory/4-eligible-students.js
@@ -7,9 +7,15 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
 */
 
-function eligibleStudents() {
+// Solution - 1
+// function eligibleStudents(arr) {
+//   return arr
+//     .filter(score => score[1] >= 8)
+//     .map(name => name[0]);
+// }
 
-}
+// Solution - 2
+const eligibleStudents = (arr) => arr.filter(trainee => trainee[1] >= 8).map(trainee => trainee[0]);
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
@@ -25,14 +31,14 @@ const attendances = [
 const util = require('util');
 
 function test(test_name, actual, expected) {
-    let status;
-    if (util.isDeepStrictEqual(actual, expected)) {
-        status = "PASSED";
-    } else {
-        status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
-    }
+  let status;
+  if (util.isDeepStrictEqual(actual, expected)) {
+    status = "PASSED";
+  } else {
+    status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
+  }
 
-    console.log(`${test_name}: ${status}`);
+  console.log(`${test_name}: ${status}`);
 }
 
 test("eligibleStudents function works",
